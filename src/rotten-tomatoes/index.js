8ab4bf162681d35bbd5ecc8e95c920c2966752ed
Nwike Ugochukwu <bondesvick@gmail.com>
2020-05-20 15:39:09 +0100

    added comments

--- src/rotten-tomatoes/index.js
+++ src/rotten-tomatoes/index.js
@@ -1,6 +1,8 @@
 function rottenTomatoes(grid) {
     let gridCopy = grid
 
+  // An array that holds added values to the four adjecent nodes
+  // top, bottom, left and right respectively.
     const dimensions = [
       [-1, 0],
       [1, 0],
@@ -14,11 +16,18 @@ function rottenTomatoes(grid) {
     let queue = [];
     let steps = 0;
 
+  // A while loop that keeps running-
+  // so far as there are still possible adjacent fresh tomato
     while (!completed) {
 
+      // Looping through the grid, seacrching for rotten tomatos-
+      // that has adjacent fresh tomatoes
       gridCopy.forEach((element, rowI) => {
           element.forEach((item, colI) => {
 
+            // If the present tomato is rotten,
+            // it checks to see if it'll find adjacent fresh tomato
+            // and pushes its postion into the queue
               if (item == 2) {
 
                   dimensions.forEach(position => {
@@ -26,7 +35,13 @@ function rottenTomatoes(grid) {
                       row = rowI + position[0];
                       col = colI + position[1];
 
-                    if (!(row < 0) && !(col < 0) && !(row >= gridCopy.length) && !(col >= gridCopy[0].length)) {
+                    // Avoiding undefind values(out of boundry positions)
+                    if (!(row < 0) && !(col < 0) &&
+                      !(row >= gridCopy.length) &&
+                      !(col >= gridCopy[0].length)) {
+
+                      // If the tomato at the current position is fresh,
+                      // its position is then pushed into the queue
                       if (gridCopy[row][col] == 1) {
                         queue.push([row, col])
                       }
@@ -37,16 +52,22 @@ function rottenTomatoes(grid) {
           })
       });
 
+      // Checks if queue is empty; meaninig that no other adjacent fresh tomato was found
         if (queue.length == 0) {
             completed = true
         } else {
-            steps++
-            queue.forEach(item => gridCopy[item[0]][item[1]] = 2);
+          // If the quene is not empty, then there are still more possible fresh tomatos
+          steps++
+          // Then they're made to rot one after another
+          queue.forEach(item => gridCopy[item[0]][item[1]] = 2);
         }
         queue = []
 
     }
 
+  // When the while loop completes its search for adjacent fresh tomatos
+  // the grid is looped again to check if there are still any fresh tomato(1) remainig
+  // if it finds one, it sets the value of steps to -1
     gridCopy.forEach(element => {
         element.forEach(item => {
             if(item == 1) steps = -1
